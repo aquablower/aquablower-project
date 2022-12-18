b6e17d847424e01f5b13cded71366029b51c2f76
aquablower <120833293+aquablower@users.noreply.github.com>
2022-12-18 20:03:37 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-var h = document.documentElement,
-    b = document.body,
-    st = 'scrollTop',
-    sh = 'scrollHeight',
-    progress = document.querySelector('.progress'),
-    scroll;
+var logo = document.querySelector('.nav.menu-logo');
+var menu = document.querySelector('.mobile.nav .nav.menu');
+var links = menu.querySelectorAll('.menu-link');
+var quitLogo = menu.querySelector('.nav .quit-logo');
 
-document.addEventListener('scroll', function () {
-    scroll = (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
-    progress.style.setProperty('--scroll', scroll + '%');
+var docE = document.documentElement
+
+logo.addEventListener('click', () => {
+    menu.style.display = "flex";
+    docE.style.overflowY = "hidden";
 });
+
+quitLogo.addEventListener('click', () => {
+    menu.style.display = "none";
+    docE.style.overflowY = "scroll";
+});
+
+links.forEach(link => link.addEventListener('click', () => {
+    menu.style.display = "none";
+    docE.style.overflowY = "scroll";
+}));
