8504b430fbe6f7088e4db67a24d1bda9ff881a13
aquablower <120833293+aquablower@users.noreply.github.com>
2022-12-19 21:33:56 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,39 @@
 var logo = document.querySelector('.nav.menu-logo');
-var menu = document.querySelector('.mobile.nav .nav.menu');
+var mobileNav = document.querySelector('.mobile.nav');
+var desktopNav = document.querySelector('.desktop.nav');
+var menu = mobileNav.querySelector('.nav.menu');
 var links = menu.querySelectorAll('.menu-link');
 var quitLogo = menu.querySelector('.nav .quit-logo');
 
-var docE = document.documentElement
+var docE = document.documentElement;
+logo.addEventListener('click', () => showMenu());
 
-logo.addEventListener('click', () => {
+quitLogo.addEventListener('click', () => removeMenu());
+
+links.forEach(link => link.addEventListener('click', () => removeMenu()));
+
+function removeMenu() {
+    menu.style.display = "none";
+    docE.style.overflowY = "scroll";
+    mobileNav.style.height = "80px";
+}
+
+function showMenu() {
     menu.style.display = "flex";
     docE.style.overflowY = "hidden";
-});
+    mobileNav.style.height = "100vh";
+}
 
-quitLogo.addEventListener('click', () => {
-    menu.style.display = "none";
-    docE.style.overflowY = "scroll";
-});
+window.onscroll = function(e) { 
+    var scrollY = window.pageYOffset || docE.scrollTop;
 
-links.forEach(link => link.addEventListener('click', () => {
-    menu.style.display = "none";
-    docE.style.overflowY = "scroll";
-}));
+    if (scrollY <= this.lastScroll) {
+        mobileNav.style.visibility = 'visible';
+        desktopNav.style.visibility = 'visible';
+    } else {
+        mobileNav.style.visibility = 'hidden';
+        desktopNav.style.visibility = 'hidden';
+    }
+
+    this.lastScroll = scrollY ;
+}
